46fefa53c8c7d046467134d2dd00578f577f44f8
john carlo franco <johncarlo_franco@yahoo.com>
2016-08-04 13:04:23 +0800

    get

--- app/src/main/java/com/apkmarvel/cameraview/MainActivity.java
+++ app/src/main/java/com/apkmarvel/cameraview/MainActivity.java
@@ -2,12 +2,15 @@ package com.apkmarvel.cameraview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
     public static final String TAG = MainActivity.class.getName();
-    protected CameraSurfaceView cameraView;
+    protected CameraTextureView cameraView;
+    private ImageView imgv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -16,8 +19,12 @@ public class MainActivity extends Activity {
         cast();
     }
     private void cast() {
-        cameraView = new CameraSurfaceView(this);
+        cameraView = new CameraTextureView(this);
         ((FrameLayout) findViewById(R.id.frame_containter)).addView(cameraView);
+        imgv = (ImageView)findViewById(R.id.imgv);
+    }
+    public void add(View v){
+        imgv.setImageBitmap(cameraView.getBitmap());
     }
 }
 
